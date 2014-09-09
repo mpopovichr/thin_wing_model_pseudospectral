84add4512ef3b4698c5854e5159f49cf17ff3df2
mpopovichr <m.popovic.hr@gmail.com>
2014-09-10 00:51:45 +0200

    for group seminar update

--- main_implicit_single_activation.py
+++ main_implicit_single_activation.py
@@ -1,5 +1,8 @@
 __author__ = 'mpopovic'
 
+import pandas as pd
+import pandas.io.sql as psql
+import pandas.io.parsers as pp
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy as sp
@@ -85,23 +88,23 @@ D2= np.dot(D,D)
 
 dt= 0.001       #time step
 
-L= -0.1      #HB interface
+L= -0.15      #HB interface
 tauT1= 3.7 #
-h0= 2./2.4  #relative to total length of wing
-k= 20.
+h0= 4./2.4  #relative to total length of wing
+k= 60.
 K1= 1.
 K2= 10.
-Gamma= 250.0
+Gamma= 300.0
 tau= 1.7 ## TODO - check!!!
 lambda1=1./tau
 #print lambda1
 #tau= 1.
-lambda2_h= -.09
-lambda2_b= -.06
+lambda2_h= -.2
+lambda2_b= -.11
 zeta_h= .0
 zeta_b= .0
-zetabar_h= 5
-zetabar_b= 2.
+zetabar_h= 15
+zetabar_b= 4
 eta_i_b= 0.
 eta_i_h= 0.
 #xi_h= 0.655
@@ -118,12 +121,12 @@ h= np.zeros(N+1)+h0
 T_minus= np.zeros(N+1)
 
 t0_a= 10.5
-sigma_t_a= 0.5
+sigma_t_a= 0.4
 sigma= 0.04
 active_space_a= np.array(map(lambda x: 1./(1+np.exp((x-L)/sigma)),x))*1./(1+np.exp(t0_a/sigma_t_a))
 active_const_a= np.array(map(lambda x: 1.,x))*1./(1+np.exp(t0_a/sigma_t_a))
-t0_i= 11.7
-sigma_t_i= .8
+t0_i= 11.8
+sigma_t_i= 0.6
 sigma= 0.04
 active_space_i= np.array(map(lambda x: 1./(1+np.exp((x-L)/sigma)),x))*1./(1+np.exp(t0_i/sigma_t_i))
 active_space_b= np.array(map(lambda x: 1./(1+np.exp((L-x)/sigma)),x))*1./(1+np.exp(t0_i/sigma_t_i))
@@ -144,11 +147,11 @@ lambda2= lambda2_h*active_space_a + lambda2_b*active_const_a
 
 #xi = xi_h*active_space_a + xi_b*active_const_a
 
-n_images= 35
-image_step= 1*1000
+n_images= 350
+image_step= 1*100
 
 for i in np.arange(n_images*image_step):
-    if i%image_step == 0:
+    if i%image_step == 100000000:
         fig= plt.figure(figsize=(20,16))
         gs= gridspec.GridSpec(3,3)
         ax0= fig.add_subplot(gs[1:,1:])
@@ -170,7 +173,7 @@ for i in np.arange(n_images*image_step):
         interpolator.set_yi(v_x)
         ax0.plot(rng, np.array(map(interpolator,rng))*3., label= 'vx')
         interpolator.set_yi(h)
-        h_plot=np.array(map(interpolator,rng))
+        h_plot=np.array(map(interpolator,rng))/h0
         avg_h_blade.append(np.sum(h_plot[HB_int:])*x_step/(rng[-1]-rng[HB_int]))
         axh.plot(range(len(avg_h_blade)),avg_h_blade)
         axh.set_xlim([0,n_images])
@@ -190,7 +193,7 @@ for i in np.arange(n_images*image_step):
         interpolator.set_yi(D.dot(v_x))
         vxx_plot= np.array(map(interpolator,rng))
         ax0.plot(rng, vxx_plot, label='v_xx')
-        interpolator.set_yi(v_yy)
+        interpolator.set_yi(v_yy/h)
         vyy_plot= np.array(map(interpolator, rng))
         ax0.plot(rng, vyy_plot, label='v_yy', color='black', linestyle='-.')
         avg_shear_blade.append(np.sum(vxx_plot[HB_int:]-vyy_plot[HB_int:])*x_step/(rng[-1]-rng[HB_int]))
@@ -206,6 +209,18 @@ for i in np.arange(n_images*image_step):
         ax0.legend()
         ax0.set_ylim([-.16,.3])
         plt.savefig('/home/mpopovic/Documents/Work/Projects/drosophila_wing_analysis/thin_wing_model_pseudospectral/implicit/test'+ fill_zeros(str(i/image_step),4)+'.png')
+        plt.close()
+    if i%image_step == 0:
+        plt.figure()
+        interpolator.set_yi(v_x)
+        plt.plot(rng, np.array(map(interpolator,rng))/2.)
+        plt.text(0.4,0.01,str(6+i*dt)[:5]+'h APF', color='orange', fontsize= 20)
+        plt.ylim(-0.04,0.015)
+        plt.xlim(-1,1)
+        plt.ylabel(r'$v_x$', fontsize=35)
+        plt.xlabel(r'$x$', fontsize=35)
+        plt.grid()
+        plt.savefig('/home/mpopovic/Documents/Work/Projects/drosophila_wing_analysis/thin_wing_model_pseudospectral/vx/test'+fill_zeros(str(i/image_step),4)+'.png', bbox_inches='tight')
         plt.close()
     dxvx= D.dot(v_x)
     a0= dt*dxvx + dt*v_yy/h + Q_plus - dt*v_x*D.dot(Q_plus)
@@ -227,15 +242,207 @@ for i in np.arange(n_images*image_step):
     active_space_i= active_space_new_i
     zeta= zeta_h*active_space_a + zeta_b*active_const_a
     zetabar= zetabar_h*active_space_i + zetabar_b*active_const_i
-    eta_i=eta_i_h*active_space_i + eta_i_b*active_const_i
+    #eta_i=eta_i_h*active_space_i + eta_i_b*active_const_i
     #xi = xi_h*active_space_a + xi_b*active_const_a
     lambda2= lambda2_h*active_space_a + lambda2_b*active_const_a
-    h_new= -1./k*(K2*Q_plus - K1*Q_minus) + h0 + (zeta - zetabar)/k - eta_i*(dxvx + v_yy)/k
+    h_new= -1./k*(K2*Q_plus - K1*Q_minus) + h0 + (zeta - zetabar)/k #- eta_i*(dxvx + v_yy)/k
     v_yy= ((h_new - h)/dt + v_x*D.dot(h))
     h= h_new
     H= np.log(h)
-    v_x= 1./Gamma*((K1*Q_minus + K2*Q_plus + zetabar + zeta +eta_i*(dxvx + v_yy))*D.dot(H) + D.dot(K1*Q_minus + K2*Q_plus + zeta + zetabar+eta_i*(dxvx + v_yy)))
+    v_x= 1./Gamma*((K1*Q_minus + K2*Q_plus + zetabar + zeta)*D.dot(H) + D.dot(K1*Q_minus + K2*Q_plus + zeta + zetabar))
     if i%image_step == 0:
         print i*dt, np.min(v_x), np.max(active_space_a), np.max(ac_factor_space_a),np.max(active_space_i), np.min(h), np.min(T_minus), np.max(T_minus)
     v_x[0]= 0.#boundary_a(i)
     v_x[-1]= 0.#boundary_b(i)
+
+
+def smooth_plot(x, y, Nsmooth= 10, *args, **kwargs):
+    kernel= np.ones(Nsmooth)/Nsmooth
+    plt.plot(x, np.convolve(y,kernel,'same'), *args, **kwargs)
+
+df_DB_shear= pp.read_csv('/data/biophys/etournay/DB/WT_25deg_111102/shear_contrib/blade/avgDeformTensorsWide.tsv', sep='\t')
+
+print df_DB_shear.columns
+print df_DB_shear['nu_xx']*13
+print shear_xx
+T1=np.array(df_DB_shear['ShearT1_xx'])
+Q_minus_exp=np.array(df_DB_shear['Q_xx'])
+time = 16.+ np.array(df_DB_shear['time_sec']/3600.)
+dt= time[1:]-time[:-1]
+1./dt
+shear_xx= np.array(df_DB_shear['nu_xx'])[:-1]/dt
+T1_rate= T1[:-1]/dt
+print T1_rate
+print max(time)
+print avg_T_minus_blade
+Nsmooth=10
+plt.figure(figsize=[12,8])
+plt.subplots_adjust(top=0.95, bottom=0.17, left=0.18, right=0.95)
+smooth_plot(time[:-1],T1_rate, linewidth=2, label='experiment', color='red')
+plt.plot(np.arange(58,350+58,1)/10.,np.array(avg_T_minus_blade)*0.5, linewidth=2, label='theory', color='yellow')
+ax1= plt.gca()
+ax1.spines['bottom'].set_color('white')
+ax1.spines['top'].set_color('white')
+ax1.xaxis.label.set_color('white')
+ax1.tick_params(axis='x', colors='white')
+ax1.spines['left'].set_color('white')
+ax1.spines['right'].set_color('white')
+ax1.yaxis.label.set_color('white')
+ax1.tick_params(axis='y', colors='white')
+ax1.grid(color='white')
+plt.xlabel(r'$h$ APF', fontsize=35, labelpad=10)
+plt.ylabel(r'$T_{xx} [h^{-1}]$', fontsize=35,labelpad=25)
+plt.xticks(fontsize=25)
+plt.yticks(fontsize=25)
+plt.legend(loc='best', fontsize=25)
+plt.savefig('T_thin.png',transparent='true')
+plt.show()
+
+
+plt.figure(figsize=[12,8])
+plt.subplots_adjust(top=0.95, bottom=0.17, left=0.18, right=0.95)
+plt.plot(time,Q_minus_exp, linewidth=2,label='experiment', color='red')
+plt.plot(np.arange(58,350+58,1)/10.,np.array(avg_Q_minus_blade)*0.5, linewidth=2, label='theory', color='yellow')
+ax1= plt.gca()
+ax1.spines['bottom'].set_color('white')
+ax1.spines['top'].set_color('white')
+ax1.xaxis.label.set_color('white')
+ax1.tick_params(axis='x', colors='white')
+ax1.spines['left'].set_color('white')
+ax1.spines['right'].set_color('white')
+ax1.yaxis.label.set_color('white')
+ax1.tick_params(axis='y', colors='white')
+ax1.grid(color='white')
+plt.xlabel(r'$h$ APF', fontsize=35, labelpad=10)
+plt.ylabel(r'$\tilde{Q}_{xx}$', fontsize=35,labelpad=35)
+plt.xticks(fontsize=25)
+plt.yticks(fontsize=25)
+plt.legend(loc='best', fontsize=25)
+plt.savefig('Q_thin.png',transparent='true')
+
+
+plt.figure(figsize=[12,8])
+plt.subplots_adjust(top=0.95, bottom=0.17, left=0.18, right=0.95)
+smooth_plot(time[:-1],shear_xx, linewidth=2, label='experiment', color='red')
+plt.plot(np.arange(58,350+58,1)/10.,np.array(avg_shear_blade)*0.5, linewidth=2, label='theory', color='yellow')
+ax1= plt.gca()
+ax1.spines['bottom'].set_color('white')
+ax1.spines['top'].set_color('white')
+ax1.xaxis.label.set_color('white')
+ax1.tick_params(axis='x', colors='white')
+ax1.spines['left'].set_color('white')
+ax1.spines['right'].set_color('white')
+ax1.yaxis.label.set_color('white')
+ax1.tick_params(axis='y', colors='white')
+ax1.grid(color='white')
+plt.xlabel(r'$h$ APF', fontsize=35, labelpad=10)
+plt.ylabel(r'$\tilde{v}_{xx} [h^{-1}]$', fontsize=35,labelpad=35)
+plt.xticks(fontsize=25)
+plt.yticks(fontsize=25)
+plt.legend(loc='best', fontsize=25)
+plt.savefig('shear_thin.png',transparent='true')
+plt.close()
+
+plt.figure()
+plt.plot(time,Q_minus_exp)
+
+plt.show()
+
+plt.figure()
+smooth_plot(time[:-1],shear_xx)
+plt.plot(np.arange(55,350+55,1)/10.,np.array(avg_shear_blade)*0.5)
+plt.show()
+df_TQ = pd.DataFrame
+df_TQ
+df_TQ['T_xx'] = T1_rate
+df_DB_shear.to_csv('/home/mpopovic/Documents/Work/seminars/group_seminar/TQ.csv')
+
+
+
+
+
+
+
+
+
+
+
+
+len(h)
+im.shape
+inPathDB='/data/biophys/etournay/DB/'
+name='WT_25deg_111103'
+framePath= inPathDB+name+'/image_data/mutant/tag/segmentationData/frame'+fill_zeros(str(200),4) + '/original_trafo.png'
+plt.figure()
+im= plt.imread(framePath)
+plt.imshow(im)
+plt.plot(range(0,4100,200),h*1000)
+plt.show()
+
+len(time)
+len(T1_rate)
+plt.figure()
+plt.plot(time[:-1],T1_rate)
+plt.plot(time,Q_minus_exp)
+plt.show()
+import matplotlib
+fig=plt.figure(figsize=[10,10])
+fig.subplots_adjust(top=.95, bottom=.11, left=.17, right=.95)
+ax1= plt.subplot(211)
+ax1.plot(time, Q_minus_exp, 'r', linewidth=2)
+ax1.set_ylim(0, .32)
+ax1.set_ylabel(r'$\tilde{Q}_{xx}$', fontsize= 40, color='white')
+ax1.xaxis.set_tick_params(labelsize=20, color='white')
+ax1.xaxis.label.set_color('white')
+ax1.yaxis.set_tick_params(labelsize=20, color='white')
+ax1.yaxis.labelpad=18
+ax1.spines['bottom'].set_color('white')
+ax1.spines['top'].set_color('white')
+ax1.xaxis.label.set_color('white')
+ax1.tick_params(axis='x', colors='white')
+ax1.spines['left'].set_color('white')
+ax1.spines['right'].set_color('white')
+ax1.yaxis.label.set_color('white')
+ax1.tick_params(axis='y', colors='white')
+ax1.grid(color='white')
+plt.setp(ax1.get_xticklabels())
+
+ax2= plt.subplot(212, sharex=ax1)
+ax2.plot(time[:-1], np.convolve(T1_rate,np.ones(10)/10.,'same'), 'b', linewidth=2)
+ax2.set_ylabel(r'$T_{xx}$', fontsize=40)
+ax2.set_yticks([-.04,-.02,0,.02,.04])
+ax2.set_ylim(-0.05,0.05)
+ax2.xaxis.set_tick_params(labelsize=20)
+ax2.yaxis.set_tick_params(labelsize=20)
+ax2.set_xlabel(r'time [h APF]', fontsize=35)
+ax2.spines['bottom'].set_color('white')
+ax2.spines['top'].set_color('white')
+ax2.xaxis.label.set_color('white')
+ax2.tick_params(axis='x', colors='white')
+ax2.spines['left'].set_color('white')
+ax2.spines['right'].set_color('white')
+ax2.yaxis.label.set_color('white')
+ax2.tick_params(axis='y', colors='white')
+ax2.grid(color='white')
+plt.setp(ax2.get_xticklabels())
+plt.savefig('QTdelay.png', transparent=True)
+plt.show()
+
+
+plt.close()
+ax1.set_xlabel(r'time [h APF]', fontsize= 25)
+# Make the y-axis label and tick labels match the line color.
+ax1.set_ylabel(r'$\tilde{Q}_{xx}$', color='r', fontsize=25)
+ax1.set_ylim(-0.3,0.3)
+for tl in ax1.get_yticklabels():
+    tl.set_color('r')
+
+ax2 = ax1.twinx()
+ax2
+ax2.set_ylabel(r'$\tilde{T}_{xx}$', color='b', fontsize=25)
+for tl in ax2.get_yticklabels():
+    tl.set_color('b')
+plt.show()
+
+
+
